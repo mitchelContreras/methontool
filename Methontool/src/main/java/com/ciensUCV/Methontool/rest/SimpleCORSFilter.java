10d725d63dc9b72df2b1927d0322a3926e848207
MitchellContreras <mitchellcontreras@gmail.com>
2015-01-26 19:34:48 -0800

    agregado filtro rest, agregando angular para validar usuario

---
+++ Methontool/src/main/java/com/ciensUCV/Methontool/rest/SimpleCORSFilter.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.ciensUCV.Methontool.rest;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author mitchell
+ *
+ */
+@Component
+public class SimpleCORSFilter implements Filter {
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#destroy()
+	 */
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1,
+			FilterChain arg2) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+        HttpServletResponse response=(HttpServletResponse) arg1;
+        
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+ 
+        arg2.doFilter(arg0, arg1);
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+	 */
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Methontool/src/main/webapp/resources/js/MVC-Angular/login-AngularJS.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+angular.module('login', []);
+//	.controller('MainController', function ($scope, $http){
+//		$scope.consultar(correo, pass){
+//			console.log("correo es "+correo);
+//			console.log("pass es "+pass);
+//		}
+//	});
+	
+//	function pruebaAjaxRest($scope, $http){
+//		$http({
+//		    url: 'http://localhost:8080/Methontool/validarUsuario', 
+//		    method: "GET",
+//		    params: {correo: 'mitchellcontreras@gmail.com',
+//		    	pass: '123456789'}
+//		 })
+//			.success(function(data, status, headers, config){
+//				console.log("success");
+//				console.log("data "+data);
+//				console.log("status "+status);
+//				console.log("headers "+headers);
+//				console.log("config "+config);
+//			})
+//			.error(function(data, status, headers, config){
+//				console.log("success");
+//				console.log("data "+data);
+//				console.log("status "+status);
+//				console.log("headers "+headers);
+//				console.log("config "+config);
+//			})
+//	}
