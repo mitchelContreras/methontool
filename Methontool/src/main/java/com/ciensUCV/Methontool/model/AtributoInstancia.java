c20c304faf17096dd16b5aadff39984a1a785f46
MitchellContreras <mitchellcontreras@gmail.com>
2015-06-09 22:23:30 -0430

    actualizacion de atributoClase y atributoInsancia

--- Methontool/src/main/java/com/ciensUCV/Methontool/model/AtributoInstancia.java
+++ Methontool/src/main/java/com/ciensUCV/Methontool/model/AtributoInstancia.java
@@ -45,83 +45,64 @@ public class AtributoInstancia {
 				+ ", cardinalidadMin=" + cardinalidadMin + ", cardinalidadMax="
 				+ cardinalidadMax + ", value=" + value + "]";
 	}
-
+	
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public int getIdGlosario() {
 		return idGlosario;
 	}
-
 	public void setIdGlosario(int idGlosario) {
 		this.idGlosario = idGlosario;
 	}
-
 	public int getIdGlosarioRelacion() {
 		return idGlosarioRelacion;
 	}
-
 	public void setIdGlosarioRelacion(int idGlosarioRelacion) {
 		this.idGlosarioRelacion = idGlosarioRelacion;
 	}
-
 	public TipoDeDato getTipoDeDato() {
 		return tipoDeDato;
 	}
-
 	public void setTipoDeDato(TipoDeDato tipoDeDato) {
 		this.tipoDeDato = tipoDeDato;
 	}
-
 	public Medida getMedida() {
 		return medida;
 	}
-
 	public void setMedida(Medida medida) {
 		this.medida = medida;
 	}
-
 	public String getPrecision() {
 		return precision;
 	}
-
 	public void setPrecision(String precision) {
 		this.precision = precision;
 	}
-
 	public String getRangoValores() {
 		return rangoValores;
 	}
-
 	public void setRangoValores(String rangoValores) {
 		this.rangoValores = rangoValores;
 	}
-
 	public String getCardinalidadMin() {
 		return cardinalidadMin;
 	}
-
 	public void setCardinalidadMin(String cardinalidadMin) {
 		this.cardinalidadMin = cardinalidadMin;
 	}
-
 	public String getCardinalidadMax() {
 		return cardinalidadMax;
 	}
-
 	public void setCardinalidadMax(String cardinalidadMax) {
 		this.cardinalidadMax = cardinalidadMax;
 	}
-
 	public String getValue() {
 		return value;
 	}
-
 	public void setValue(String value) {
 		this.value = value;
 	}
