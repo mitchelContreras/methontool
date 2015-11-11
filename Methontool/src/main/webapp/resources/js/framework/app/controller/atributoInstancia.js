4a081bf0a1ff62df3f526b92d434588fdb2696db
MitchellContreras <mitchellcontreras@gmail.com>
2015-11-10 20:09:07 -0430

    factoryGlosario correccion en atributoInstancia

--- Methontool/src/main/webapp/resources/js/framework/app/controller/atributoInstancia.js
+++ Methontool/src/main/webapp/resources/js/framework/app/controller/atributoInstancia.js
@@ -113,7 +113,7 @@ function ControllerAtributoInstancia($rootScope,
 	                	console.log("consultar es true");
 //	                	asigno concepto
 	                	cnAtributoInstancia.varEdicion.glosarioConcepto
-	                		= FactoryGlosario.consultarElemento(aux.elemento.idGlosarioConcepto);
+	                		= buscarEnlistaGlosario(aux.elemento.idGlosarioConcepto);
 	                	
 	                	if(aux.elemento.idGlosarioConcepto == 0){
 	                		console.log("es 0 glosario");
@@ -163,8 +163,15 @@ function ControllerAtributoInstancia($rootScope,
 	                }
 	            }
 	        );
-		
-		
+	}
+	function buscarEnlistaGlosario(id){
+		console.log("buscar id:"+id+" cnAtributoInstancia.listaGlosario.length:"+cnAtributoInstancia.listaGlosario.length);
+		for (var i=0;i<cnAtributoInstancia.listaGlosario.length;i++){
+			if (cnAtributoInstancia.listaGlosario[i].id == id){
+				console.log("encontro");
+				return cnAtributoInstancia.listaGlosario[i];
+			}
+		}
 	}
 	function modificarAtributoInstancia (){
 		cnAtributoInstancia.disabled = false;
